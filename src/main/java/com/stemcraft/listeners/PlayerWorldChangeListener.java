68313be1118c6dd00fbedafd7a62d8ca142f23ca
James Collins <james.collins@outlook.com.au>
2024-12-25 11:28:03 +1000

    renamed worldsPartOfSameRealm to worldsInSameRealm

--- src/main/java/com/stemcraft/listeners/PlayerWorldChangeListener.java
+++ src/main/java/com/stemcraft/listeners/PlayerWorldChangeListener.java
@@ -22,7 +22,7 @@ public class PlayerWorldChangeListener implements Listener {
         World toWorld = player.getWorld();
         GameMode gameMode = player.getGameMode();
 
-        if (!plugin.getSTEMCraftLib().worldsPartOfSameRealm(fromWorld, toWorld)) {
+        if (!plugin.getSTEMCraftLib().worldsInSameRealm(fromWorld, toWorld)) {
             plugin.savePlayerState(player, fromWorld, gameMode);
             plugin.loadPlayerState(player, toWorld, gameMode);
         }
