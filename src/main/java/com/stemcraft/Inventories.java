d516ff8270dd3211f24be46ee5e8d500d355022b
James Collins <james.collins@outlook.com.au>
2025-01-06 10:10:54 +1000

    added areSharedWorlds method

--- src/main/java/com/stemcraft/Inventories.java
+++ src/main/java/com/stemcraft/Inventories.java
@@ -7,18 +7,44 @@ import com.stemcraft.storage.YamlPlayerDataStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Inventories extends STEMCraftPlugin {
     private static Inventories instance;
     private PlayerDataStorage storage;
+    private final List<List<String>> sharedWorlds = new ArrayList<>();
+
 
     @Override
     public void onEnable() {
         super.onEnable();
 
         instance = this;
+
+        File configFile = new File(this.getDataFolder(), "config.yml");
+        if(configFile.exists()) {
+            YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+            // Load shared inventory worlds
+            ConfigurationSection shared = config.getConfigurationSection("shared");
+            if(shared != null) {
+                shared.getKeys(false).forEach(key -> {
+                    List<String> worlds = shared.getStringList(key);
+                    if(worlds.size() < 2) {
+                        getLogger().warning("Shared inventory worlds must have at least 2 worlds.");
+                    } else {
+                        sharedWorlds.add(worlds);
+                    }
+                });
+            }
+        }
 
         // Currently this plugin only supports YAML storage
         storage = new YamlPlayerDataStorage(instance);
@@ -43,6 +69,22 @@ public class Inventories extends STEMCraftPlugin {
 
     public Inventories getInstance() {
         return instance;
+    }
+
+    /**
+     * Returns if the two worlds share inventories
+     * @param source The source world name
+     * @param target The target world name
+     * @return If the worlds share inventories
+     */
+    public boolean areSharedWorlds(String source, String target) {
+        for(List<String> worlds : sharedWorlds) {
+            if(worlds.contains(source) && worlds.contains(target)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
